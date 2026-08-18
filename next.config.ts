45cd5637f68cfd86a8fd46ac0eb7ca64f280bcb4
tamilkarma5-alt <tamilkarma5@gmail.com>
2026-08-18 16:11:16 +0530

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+```ts
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
+```
